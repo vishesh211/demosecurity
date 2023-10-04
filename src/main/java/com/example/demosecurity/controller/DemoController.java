dac4415322c9632abd85cb2af7da7870eb5a4b29
vishesh <visheshtiwari@ggx.io>
2023-10-04 23:41:14 +0530

    added leaders page

--- src/main/java/com/example/demosecurity/controller/DemoController.java
+++ src/main/java/com/example/demosecurity/controller/DemoController.java
@@ -10,4 +10,9 @@ public class DemoController {
     public String showHome() {
         return "home";
     }
+
+    @GetMapping("/leaders")
+    public String showLeaders() {
+        return "leaders";
+    }
 }
